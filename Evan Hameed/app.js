3b1d212b39a3e3a03aea3b906a1c0a5f98645731
evan hameed <evanhameed9966@gmail.com>
2020-05-17 01:42:10 +0300

    Adding the event listner with ability to add LIs

--- Evan Hameed/app.js
+++ Evan Hameed/app.js
@@ -6,11 +6,26 @@ currentDate.innerText ="Today : "+ date.toDateString();
 
 
 let todoButton=document.getElementById('todoBtn');
-let todo=document.getElementById('todo');
 let deadline=document.getElementById('deadline');
 let list=document.getElementById('list');
+let todo=document.getElementById('todo');
+let form=document.getElementById('myForm');
+
 
 todoButton.addEventListener('click',function(){
+    if(todo &&todo.value &&deadline &&deadline.value){
+let listItem= document.createElement('li');
+let todoTitle=document.createElement('span');
+let todoDeadline=document.createElement('span');
+listItem.classList.add('listItem');
+list.appendChild(listItem);
 
+todoTitle.innerText= todo.value;
+listItem.appendChild(todoTitle);
+todoDeadline.innerText=deadline.value;
+listItem.appendChild(todoDeadline);
+
+form.reset();
+    }
 
 });
