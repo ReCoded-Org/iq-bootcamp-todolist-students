95a8507d44cf1f6dfc54b90e8eea123d20975713
evan hameed <evanhameed9966@gmail.com>
2020-05-16 22:51:21 +0300

    showing the current date at the top of the webpage

---
+++ Evan Hameed/app.js
@@ -0,0 +1,5 @@
+
+// setting the Current date we see up in our webpage
+let currentDate = document.getElementById('currentDate');
+let date = new Date();
+currentDate.innerText ="Today : "+ date.toDateString();
