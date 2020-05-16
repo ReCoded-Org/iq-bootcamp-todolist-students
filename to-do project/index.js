dffd66969a2054f93b298bf525de986f9b0141a4
Yara Alkassar <yara.sarmad@yahoo.com>
2020-05-17 01:01:21 +0300

    Added a clock

--- to-do project/index.js
+++ to-do project/index.js
@@ -3,7 +3,14 @@ function getdate(){
 let today = new Date();
 let d = String(today.getDate());
 let m = String(today.getMonth() + 1); //months start with 0
-let y = today.getFullYear();
+let y = String(today.getFullYear());
 today = d + '/' + m + '/' + y;
 document.getElementById('DateOfToday').innerText= today;
+}
+window.addEventListener("load", Time());
+function Time(){
+    let time = new Date();
+let t= time.toLocaleTimeString();
+document.getElementById('Time').innerText= t;
+run =setTimeout(Time,1000);
 }
