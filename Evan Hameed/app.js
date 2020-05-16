9b9aa73c2d913c3001cf2d7495a21874d92e4d57
evan hameed <evanhameed9966@gmail.com>
2020-05-16 23:26:33 +0300

    Adding a list variable

--- Evan Hameed/app.js
+++ Evan Hameed/app.js
@@ -8,8 +8,9 @@ currentDate.innerText ="Today : "+ date.toDateString();
 let todoButton=document.getElementById('todoBtn');
 let todo=document.getElementById('todo');
 let deadline=document.getElementById('deadline');
+let list=document.getElementById('list');
 
 todoButton.addEventListener('click',function(){
 
-    
+
 });
