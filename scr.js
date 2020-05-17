9ed7df6e634ac0f2638bd561390a34e0b6aa038e
zeena kareem <zena342699@gmail.com>
2020-05-17 16:09:41 +0300

    basic_todo

---
+++ scr.js
@@ -0,0 +1,60 @@
+
+var d = new Date();
+
+let h=document.getElementById("h");
+h.innerHTML=d.getDate()+'/'+(d.getMonth()+1)+'/'+d.getFullYear();
+
+let in1=document.getElementById("input1") ;
+
+let new_digit_month=(d.getMonth()+1);
+
+if ((d.getMonth()+1)<10 ) {
+    new_digit_month = "0" +(d.getMonth()+1) ;
+} 
+let new_digit_day=d.getDate() ;
+
+if (d.getDate()<10 ) {
+    new_digit_day = "0" +d.getDate() ;
+} 
+let m=d.getFullYear()+'-'+new_digit_month+'-'+new_digit_day;
+in1.setAttribute('min',m);
+
+//list
+function makelist() {
+  if (document.getElementById("input2").value==''){
+    document.getElementById("input2").required;
+  }
+  else{
+ var tag1 = document.createElement("li");
+ var element = document.getElementById("list");
+ element.appendChild(tag1);
+console.log(tag1)
+
+             var tag2=document.createElement("h4");
+              //var text1=document.createTextNode(document.getElementById("input2").value);
+             tag2.innerHTML=document.getElementById("input2").value;
+             
+             tag1.appendChild(tag2);
+              
+
+             var tag3=document.createElement("p");
+              text2=document.createTextNode(document.getElementById("input1").value);
+              tag3.appendChild(text2);
+              tag1.appendChild(tag3)
+              
+         
+              
+ 
+ 
+
+
+
+ document.getElementById("input1").value='';
+ document.getElementById("input2").value='';
+}
+}
+
+
+
+
+
