6508e8ade43916e6a7c443ea425ec8940d1833ee
Hussien-Talal-Majeed <hossientalal2@gmail.com>
2020-05-17 21:27:54 +0300

    changing html layout

--- Hussein-Talal/index.js
+++ Hussein-Talal/index.js
@@ -1,7 +1,7 @@
 //adding the date to the header
 const date = document.getElementById('date');
 const day = new Date().getDate();
-const month = new Date().getMonth() + 1;
+const month = (new Date().getMonth() + 1);
 const year = new Date().getFullYear();
 date.textContent = `${day}/${month}/${year}` ;
 
