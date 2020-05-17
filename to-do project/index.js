87858f6c3cc3fa0e69c4b93c916fbe38e069c87e
Yara Alkassar <yara.sarmad@yahoo.com>
2020-05-18 00:41:16 +0300

    Added a function to Restrict old dates

--- to-do project/index.js
+++ to-do project/index.js
@@ -4,6 +4,12 @@ function getdate() {
     let d = String(today.getDate());
     let m = String(today.getMonth() + 1); //months start with 0
     let y = String(today.getFullYear());
+    if (d < 10) {
+        d = `0${d}`;
+    }
+    if (m < 10) {
+        m = `0${m}`;
+    }
     today = y + '/' + m + '/' + d;
     document.getElementById('DateOfToday').innerText = today;
 }
@@ -13,6 +19,25 @@ function Time() {
     let t = time.toLocaleTimeString();
     document.getElementById('Time').innerText = t;
     let run = setTimeout(Time, 1000);
+}
+
+window.addEventListener("load", daterestrict);
+function daterestrict() {
+    let deadlineDate = document.getElementById('date');
+    let today = new Date();
+    let d = String(today.getDate());
+    let m = String(today.getMonth() + 1);
+    let y = String(today.getFullYear());
+    //here the days and months should have a 0 before them so it gives the correct min value.
+    if (d < 10) {
+        d = `0${d}`;
+    }
+    if (m < 10) {
+        m = `0${m}`;
+    }
+    today = y + '-' + m + '-' + d;
+    deadlineDate.setAttribute('title', `Add date from: ${today}`);
+    deadlineDate.setAttribute('min', today);
 }
 
 let btn = document.getElementById('btn'); //get button
@@ -33,7 +58,6 @@ function addToList() {
     else {
         listText = taskName + "<br />" + deadlineDate;
     }
-
     listItem.innerHTML = listText;
     list.appendChild(listItem);
     let form = document.getElementById('taskForm');
