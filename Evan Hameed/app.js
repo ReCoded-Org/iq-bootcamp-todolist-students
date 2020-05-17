e95fb8665e811650908969d91190aefaddd605b6
evan hameed <evanhameed9966@gmail.com>
2020-05-18 01:05:14 +0300

    Storing input data in the localSorage

--- Evan Hameed/app.js
+++ Evan Hameed/app.js
@@ -13,8 +13,8 @@ let form=document.getElementById('myForm');
 let noTasksText=document.getElementById('luckyYou');
 let ulSection = document.getElementsByClassName('ulSection');
 
-
-todoButton.addEventListener('click',function(){
+let tasks = [];
+todoButton.addEventListener('click',function(e){
 if(todo &&todo.value &&deadline &&deadline.value){
     noTasksText.innerText=" ohh now you have some !!"
     
@@ -29,7 +29,22 @@ if(todo &&todo.value &&deadline &&deadline.value){
     todoDeadline.innerText=deadline.value;
     listItem.appendChild(todoDeadline);
 
+    
+    e.preventDefault();
+    let task = {
+        title : todo.value,
+        deadline: deadline.value
+
+    };
+    tasks.push(task);
+    console.log(tasks);
+    
     form.reset();
+
+    localStorage.setItem('myTasksList' , JSON.stringify(tasks));
+    localStorage.setItem('key(could be myTasksList)' , JSON.stringify(NameOftheArrayThatHasAllTheTaskObjects));
+
+
      }
 
 });
