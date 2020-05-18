115074b2f8cd55fd8670bbd95ec2ef18d186e146
Arvin Alhissou <arvin.alhissou@gamil.com>
2020-05-18 07:07:36 +0300

    the function of the input and add them

--- Arvin/main.js
+++ Arvin/main.js
@@ -14,3 +14,40 @@ var todos = JSON.parse(todosEX);
 var date = new Date();
 document.getElementById("time").innerHTML = date.toDateString();
 
+class item{
+	constructor(name){
+		this.createItem(name);
+	}
+    createItem(name){
+    	var itemBox = document.createElement('div');
+        itemBox.classList.add('item');
+
+    	var input = document.createElement('input');
+    	input.type = "text";
+    	input.disabled = true;
+    	input.value = name;
+        input.classList.add('item_input');
+        // for date
+        var datte = document.createElement('input');
+    	datte.type = "date";
+    	datte.disabled = true;
+    	datte.value = name;
+    	datte.classList.add('item_input');
+
+    	var edit = document.createElement('button');
+    	edit.classList.add('edit');
+    	edit.innerHTML = "EDIT";
+    	edit.addEventListener('click', () => this.edit(input, name));
+
+    	var remove = document.createElement('button');
+    	remove.classList.add('remove');
+    	remove.innerHTML = "REMOVE";
+    	remove.addEventListener('click', () => this.remove(itemBox, name));
+
+    	container.appendChild(itemBox);
+
+        itemBox.appendChild(input);
+        itemBox.appendChild(edit);
+        itemBox.appendChild(remove);
+
+    }
