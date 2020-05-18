6b67fd3045e6b270e5de2948660c821289a4ca6e
Hussien-Talal-Majeed <hossientalal2@gmail.com>
2020-05-18 04:20:09 +0300

    organizing the code and adding coments

--- Hussein-Talal/index.js
+++ Hussein-Talal/index.js
@@ -1,28 +1,29 @@
-//adding the date to the header
+//declaring the variables
 const todayDate = document.getElementById('todayDate');
 let day = new Date().getDate();
 let month = (new Date().getMonth() + 1);
 let year = new Date().getFullYear();
-todayDate.textContent = `${day}/${month}/${year}` ;
-
-
 const form = document.getElementById('form');
 let text = document.getElementById('text');
 const ul = document.querySelector('ul');
 let date = document.getElementById('date');
 let numberOfTasks = 1;
 const msg = document.getElementById('msg');
+
+//adding the date to the header
+todayDate.textContent = `${day}/${month}/${year}` ;
+
+// setting the min attribute in date input field
 if(day<10){
     day='0'+day
 } 
 if(month<10){
     month='0'+month
 } 
-
 let today = year+'-'+month+'-'+day;
-console.log(today);
-
 date.setAttribute("min", today);
+
+//adding-task function
 function addTask(){
     if (text.value === '' || date.value === '') {
         msg.textContent = "Input the task and the date";
@@ -30,12 +31,11 @@ function addTask(){
     else{
         document.getElementById('noTasks').textContent = `You have ${numberOfTasks} task/s`;
         const add = document.createElement('li');
-        let textNode = document.createTextNode(text.value + " your deadline is on " + date.value);
+        let textNode = document.createTextNode(text.value + " deadline :" + date.value);
         add.appendChild(textNode);
         ul.appendChild(add);
         numberOfTasks++;
         form.reset();
         msg.textContent = '';
-    }
-    
+    }    
 }
