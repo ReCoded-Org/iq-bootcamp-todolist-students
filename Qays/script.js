2ccfc11d5357f285aa4564f1e8510c058ff17967
Qays <work.worker55@gmail.com>
2020-05-18 15:29:01 +0300

    Version 1 before theming

---
+++ Qays/script.js
@@ -0,0 +1,26 @@
+let currentDate = new Date();
+
+let dateH1 = document.getElementById('current-date');
+
+dateH1.innerText = currentDate.getDate() + "/" + (currentDate.getMonth() +1) +"/"+currentDate.getFullYear();
+
+let deadlineInput = document.getElementById('deadline');
+
+deadlineInput.min =  currentDate.toISOString().split("T")[0];
+console.log(currentDate.toISOString());
+
+function addTask(event) {
+    let description = document.getElementById('description').value;
+    let deadline = deadlineInput.value;
+    if (description ==='' || deadline ==='' ) {
+        alert("Make sure to fill all of the fields");
+        return;
+      } 
+    let task = document.createElement('li');
+    task.innerHTML = `<div>${description}</div><div>${deadline}</div>`;
+    let tasksList = document.getElementById('tasks-list');
+    tasksList.appendChild(task);
+    document.getElementById('deadline').value = '';
+    document.getElementById('description').value = '';
+    document.getElementById('no-tasks-message').remove();
+}
