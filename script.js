11d88f07c35cd705c2537f9667ebc54c1ddc45a9
Ahmed-Alassaf <ahmedfaysal199@gmail.com>
2020-05-18 14:20:36 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let n = new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("ToDate")
+.innerHTML = m + "/" + d +"/" + y;
+
+
+document.getElementById("addTodo").addEventListener('click', function(toDoEvent) {
+    let text = document.getElementById("text").value;
+    let deadline = document.getElementById("deadline").value;
+    let addedToDos = document.getElementById("addedToDos");
+    if (text == "", deadline == "") {
+        alert("Add a ToDo!");
+    }
+
+    else { 
+        let newToDo = document.createElement('li');
+        newToDo.innerHTML = `<div>${text}</div><div>${deadline}</div>`;
+        addedToDos.appendChild(newToDo);
+
+        text = document.getElementById('text').value ='';
+        deadline = document.getElementById('deadline').value ='';
+        
+    }
+});
