6b316bf97289935b4a10413702d279e6bc446402
evan hameed <evanhameed9966@gmail.com>
2020-05-18 13:23:54 +0300

    Adding object of task and an array of tasks

--- Evan Hameed/app.js
+++ Evan Hameed/app.js
@@ -5,11 +5,11 @@ let date = new Date();
 currentDate.innerText ="Today : "+ date.toDateString();
 
 
-let todoButton=document.getElementById('todoBtn');
+const todoButton=document.getElementById('todoBtn');
 let deadline=document.getElementById('deadline');
 let list=document.getElementById('list');
 let todo=document.getElementById('todo');
-let form=document.getElementById('myForm');
+const form=document.getElementById('myForm');
 let noTasksText=document.getElementById('luckyYou');
 let ulSection = document.getElementsByClassName('ulSection');
 
@@ -17,33 +17,23 @@ let tasks = [];
 todoButton.addEventListener('click',function(e){
 if(todo &&todo.value &&deadline &&deadline.value){
     noTasksText.innerText=" ohh now you have some !!"
-    
-    let listItem= document.createElement('li');
-    let todoTitle=document.createElement('span');
-    let todoDeadline=document.createElement('span');
-    listItem.classList.add('listItem');
-    list.appendChild(listItem);
 
-    todoTitle.innerText= todo.value;
-    listItem.appendChild(todoTitle);
-    todoDeadline.innerText=deadline.value;
-    listItem.appendChild(todoDeadline);
+    let task={
+    Title: todo.value,
+    Deadline: deadline.value
+            };
+let listItem = document.createElement('li');
+let taskTitle = document.createElement('span');
+taskTitle.innerText = task.Title;
+let taskDeadline = document.createElement('span');
+taskDeadline.innerText= task.Deadline;
+listItem.appendChild(taskTitle);
+listItem.appendChild(taskDeadline);
+listItem.classList.add('listItem');
+list.appendChild(listItem);
+tasks.push(task);
 
-    
-    e.preventDefault();
-    let task = {
-        title : todo.value,
-        deadline: deadline.value
-
-    };
-    tasks.push(task);
-    console.log(tasks);
-    
-    form.reset();
-
-    localStorage.setItem('myTasksList' , JSON.stringify(tasks));
-    localStorage.setItem('key(could be myTasksList)' , JSON.stringify(NameOftheArrayThatHasAllTheTaskObjects));
-
+form.reset();
 
      }
 
