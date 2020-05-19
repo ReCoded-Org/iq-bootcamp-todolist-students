49d02b36e410bdad7246c843393d4028cc7c4bd6
shna rafeeq <shnar.majeed@gmail.com>
2020-05-19 17:48:10 +0300

    calender-modified

--- shna-rafeeq/index.js
+++ shna-rafeeq/index.js
@@ -5,18 +5,22 @@ var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 var yyyy = today.getFullYear();
 today = mm + '/' + dd + '/' + yyyy;
 document.getElementById("header").innerHTML = today;
-/*Main*/
+
+const dateElemnt = document.getElementById("get-date");
+dateElemnt.setAttribute('min', `${yyyy}-${mm}-${dd}`);
 
 let listUl = document.getElementById("list-ul");
-let h1 = document.getElementById("f-h1");
 let p = document.getElementById("pul");
+
+
 
 function newLiAdded(){
     let gettingTodo = document.getElementById("get-todo").value; 
     let getdate =document.getElementById("get-date").value;
+    let priorities = document.getElementById("drop-list").value;
     let frm = document.getElementsByName('contact-form')[0];
     let li=document.createElement('li');
-    li.innerText= gettingTodo +" \t "+getdate;
+    li.innerHTML = `<div> <input type="checkbox" class="checked"> ${gettingTodo} ${getdate} ${priorities} </div> `;
     let checkFilltext = document.forms["contact-form"]["select-todo"].value;
     let checkFillDtae = document.forms["contact-form"]["selecte-date"].value;
     if (checkFilltext == "" || checkFilltext == null || checkFillDtae == "" || checkFillDtae == null) {
@@ -24,7 +28,7 @@ function newLiAdded(){
         return false;
       }
     p.remove();
-    li.style.fontSize = "30px";
+   
     listUl.appendChild(li);
     frm.reset();
 }
