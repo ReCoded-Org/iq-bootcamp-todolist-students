fa82c84d224e94296cb6a551795cdca3bb5d5d4f
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-05-20 04:21:56 +0300

    adding delete button

--- abdulbasit-web/script.js
+++ abdulbasit-web/script.js
@@ -1,10 +1,11 @@
 const input = document.querySelector("#input-task");
 const btnAdd = document.querySelector("#btn-add");
-const btnDelete = document.querySelector(".fa-trash-alt");
+const btnDelete = document.querySelector("i.delete");
 const ul = document.querySelector("#task-list");
 const taskCount = document.querySelector("#task-counter");
 const datePicker = document.querySelector("#date-picker");
-// console.log(datePicker.value);
+
+
 
 btnAdd.addEventListener("click", (e) => {
   e.preventDefault();
@@ -15,36 +16,38 @@ btnAdd.addEventListener("click", (e) => {
     const task = document.createElement("li");
     task.className = 
       "list-group-item my-2 rounded-pill";
-    task.id = "task";
     task.innerHTML = `<div class="d-flex justify-content-between align-items-center ">
         <div class="d-flex align-items-center ">
             <i class="far fa-circle mr-3 pointer" ></i>
             <p class="m-0">${todo} .</p>
         </div>
-        <div>
-            <i  class="far fa-trash-alt text-danger  ml-3 pointer" ></i>
+        <div class="icon">
+            
         </div>
     </div>
     <small class="mb-1 pl-3 ml-3 text-muted ">${deadline}</small>`;
 
+    const deleteIcon = document.createElement("i")
+    deleteIcon.className="far fa-trash-alt text-danger delete  ml-3 pointer"
+    
     // task.appendChild(btn);
     ul.appendChild(task);
+    document.querySelector('.icon').appendChild(deleteIcon);
 
     //clear input after adding
     input.value = "";
 
     //count tasks
     taskCount.textContent = `${ul.childElementCount}`;
+    //delete task function
+    deleteIcon.addEventListener("click" , ()=>{
+        task.remove();
+    });
   }
-
-  btnDelete.addEventListener("click", () => {
-    document.querySelector(".rounded-pill").remove();
-  //   console.log("work");
   
-  });
+
 });
 
-//delete task function
 
 //data and time setter
 const day = document.querySelector("#day");
@@ -84,6 +87,10 @@ $(document).ready(function () {
   $(btnAdd).click(() => {
     $(input).fadeIn("slow");
     $(datePicker).fadeIn("slow");
+  });
+
+  $(btnDelete).click(()=>{
+    console.log("work");
   });
 });
 
