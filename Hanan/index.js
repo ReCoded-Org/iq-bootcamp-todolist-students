65ee0018cf1508194f697054f70d168dd9b435c8
Hanan Dlshad <ensw03217@student.su.edu.krd>
2020-05-20 14:52:38 +0300

    add new item

--- Hanan/index.js
+++ Hanan/index.js
@@ -1,26 +1,63 @@
 /*Header*/
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0');
-var yyyy = today.getFullYear();
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0');
+let yyyy = today.getFullYear();
 today = dd + '/' + mm + '/' + yyyy;
 document.getElementById("headerH1").innerHTML = today;
 /*Main*/
 let listUl = document.getElementById("ul-list");
-let p1 = document.getElementById("ul-h");
+let h2 = document.getElementById("ul-h");
+
+let settingMin =  document.getElementById("select-date");
+settingMin.setAttribute('min',`${yyyy}-${mm}-${dd}`);
+
+let h3=document.createElement("h3");
+
+let count=0;
+let listLiId=0;
+
 function newLi(){
     let gettingTodo = document.getElementById("write-todo").value; 
-    let gettingdate =document.getElementById("select-date").value;
+    let gettingdate =  document.getElementById("select-date").value;
+    let priority =  document.getElementById("add-priorities").value;
     let formInput = document.getElementsByName('formName')[0];
     let li=document.createElement('li');
-    li.innerText= gettingTodo +"\n "+gettingdate;
+
+
+    li.innerHTML=`<div id="list-item-all-content-${listLiId}"><div><input type="checkbox" id="list-item-checkbox-${listLiId}"  onChange="checkboxChanged(${listLiId})"> <span id="list-item-${listLiId}">${gettingTodo} </span> </div>
+    <div>${gettingdate }   ${ priority} </div><div display="inline;"><input type="button" id="list-item-button-${listLiId}" onclick="remove(${listLiId})" value="Remove" ></div></div>`;
+
+
     let checkFilltext = document.forms["formName"]["inputText"].value;
     let checkFillDtae = document.forms["formName"]["selecteDate"].value;
     if (checkFilltext == "" || checkFilltext == null || checkFillDtae == "" || checkFillDtae == null) {
-        alert("Must Be All Filled Out");
+        alert("⚠️ Must Be All Filled Out");
         return false;
       }
-    p1.remove();
-    listUl.appendChild(li);
-    formInput.reset();
+     
+      count=count+1;
+      h3.innerText=`There Is ${count} Task(s)`;
+      formInput.appendChild(h3);
+      h2.remove();
+      listUl.appendChild(li);
+      formInput.reset();
+      listLiId++;
+}
+
+function checkboxChanged(id){
+  let Box = document.getElementById("list-item-checkbox-"+id);
+  let paragraph = document.getElementById("list-item-"+id);
+  if (Box.checked == true){
+    paragraph.style.textDecoration = "line-through";
+  } else {
+    paragraph.style.textDecoration = "none";
+  } 
+}
+
+function remove(id) {
+  let button = document.getElementById("list-item-button-"+id);
+  let allLi = document.getElementById("list-item-all-content-"+id);
+
+  allLi.remove();
 }
