2c930953f77ad74b08ab7e88d12ecb96abd29a97
Mahmoud Mansour <mahmodlte@gmail.com>
2020-05-21 20:52:48 +0300

    add new things

--- Mahmoud Mansour/js/index.js
+++ Mahmoud Mansour/js/index.js
@@ -1,3 +1,15 @@
+let todo = [];
+const unList = document.getElementById('list');
+const listItem = document.createElement('li');
+
+const taskValue = document.getElementById("todo-input").value;
+const deadlineValue = document.getElementById("todo-deadline").value;
+const priortyValue = document.getElementById("Priorties").value;
+const form = document.getElementById("task-form");
+const add = document.getElementById("addBtn");
+
+
+
 function Today (){
     let dateH1 = document.getElementById('header');
     let todayH1 = document.getElementById('today');
@@ -10,33 +22,17 @@ function Today (){
 }
 Today();
 
-function addItem (){
-    let taskValue = document.getElementById("todo-input").value;
-    let unList = document.getElementById('list');
-    let listItem = document.createElement('li');
-    let deadLineValue = document.getElementById("todo-deadline").value;
-    let deadlineView = document.createElement('p')
-    let emptyStatus = document.getElementById('no-tasks');
-   
-    listItem.style.listStyle = 'none'
-    listItem.style.borderBottom = '1px solid #eee'
-    listItem.style.fontSize = '1.6rem'
-    listItem.innerText = taskValue;
-    deadlineView.innerHTML = deadLineValue;
 
-    if (taskValue === "") {
-        alert("Enter Task!")
 
-    } else {
-        
-        emptyStatus.style.display = 'none';
-        unList.appendChild(listItem);
-        listItem.appendChild(deadlineView);
-       
-      
-
+function addItem (task,deadline,priorty){
+    const todos = {
+        task,
+        deadline,
+        priorty,
+        checked: false,
+        id: Date.now()
+    };
+    todo.push(todos);
+    console.log(todo);
     }
-    
-    document.getElementById('task-form').reset();
-
-}
+  
