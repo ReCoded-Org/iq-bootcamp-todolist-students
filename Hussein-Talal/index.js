f460ca23396de009671e73cc533d1b8325154ff1
Hussien-Talal-Majeed <hossientalal2@gmail.com>
2020-05-21 14:33:16 +0300

    organizing the code

--- Hussein-Talal/index.js
+++ Hussein-Talal/index.js
@@ -4,9 +4,10 @@ let day = new Date().getDate();
 let month = (new Date().getMonth() + 1);
 let year = new Date().getFullYear();
 const form = document.getElementById('form');
-let text = document.getElementById('text');
+const text = document.getElementById('text');
 const priority = document.getElementById('priority');
-let date = document.getElementById('date');
+const submit = document.getElementById('submit');
+const date = document.getElementById('date');
 const msg = document.getElementById('msg');
 const ul = document.querySelector('ul');
 
@@ -14,7 +15,8 @@ const ul = document.querySelector('ul');
 
 todayDate.textContent = dateFormater(new Date().toDateString().split(" ")); 
 function dateFormater (dateString){
-    return dateString[0]+" "+dateString[1]+ " "+dateString[2] +","+dateString[3];
+    const dateFormated = dateString[0]+" "+dateString[1]+ " "+dateString[2] +","+dateString[3] 
+    return dateFormated ;
 }
 
 
@@ -33,7 +35,6 @@ function addTask(){
         }
         tasks.push(taskObj);
         document.getElementById('noTasks').textContent = `You have ${numberOfTasks} task/s`;
-        //const div = document.createElement('div');
         taskPrint(taskObj);
         numberOfTasks++;
         form.reset();
@@ -52,10 +53,13 @@ function taskPrint (taskObj){
 }
 console.log(tasks);
 console.log(priority.value);
-const submit = document.getElementById('submit');
+
 submit.addEventListener('click', (e)=>{
     e.preventDefault();
 })
+
+
+
 
 //initializing materialize components 
 const calendar = document.querySelector('.datepicker');
