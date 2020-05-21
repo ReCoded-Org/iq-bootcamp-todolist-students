8e8b5706eddeeeb02289c79e710702c59ded01fa
Hussien-Talal-Majeed <hossientalal2@gmail.com>
2020-05-21 05:37:48 +0300

    adding tasks array of objects and function to print it

--- Hussein-Talal/index.js
+++ Hussein-Talal/index.js
@@ -5,52 +5,53 @@ let month = (new Date().getMonth() + 1);
 let year = new Date().getFullYear();
 const form = document.getElementById('form');
 let text = document.getElementById('text');
-const ul = document.querySelector('ul');
+const priority = document.getElementById('priority');
 let date = document.getElementById('date');
 const msg = document.getElementById('msg');
+const ul = document.querySelector('ul');
 
 //adding the date to the header
 
 todayDate.textContent = dateFormater(new Date().toDateString().split(" ")); 
-
 function dateFormater (dateString){
     return dateString[0]+" "+dateString[1]+ " "+dateString[2] +","+dateString[3];
 }
 
-// setting the min attribute in date input field
-// if(day<10){
-//     day='0'+day
-// } 
-// if(month<10){
-//     month='0'+month
-// } 
-// let today = year+'-'+month+'-'+day;
-// date.setAttribute("min", today);
 
 //adding-task function
 let numberOfTasks = 1;
+const tasks = [];
 function addTask(){
     if (text.value === '' || date.value === '') {
         msg.textContent = "Input the task and the date";
     }
     else{
+        const taskObj = {
+            task : text.value,
+            deadline : date.value,
+            priority : priority.value
+        }
+        tasks.push(taskObj);
         document.getElementById('noTasks').textContent = `You have ${numberOfTasks} task/s`;
-        const div = document.createElement('div');
-        const li = document.createElement('li');
-        const span = document.createElement('span');
-        //div.className = "todo-item";
-        span.innerText = "deadline : " + date.value;
-        li.className = "collection-item";
-        li.innerText = text.value;
-        li.appendChild(span);
-        //div.appendChild(li);
-        ul.appendChild(li);
+        //const div = document.createElement('div');
+        taskPrint(taskObj);
         numberOfTasks++;
         form.reset();
         msg.textContent = '';
     }    
 }
 
+function taskPrint (taskObj){
+    const li = document.createElement('li');
+        const span = document.createElement('span');
+        span.innerText = "deadline : " + taskObj.deadline;
+        li.className = "collection-item";
+        li.innerText = taskObj.task;
+        li.appendChild(span);
+        ul.appendChild(li);
+}
+console.log(tasks);
+console.log(priority.value);
 const submit = document.getElementById('submit');
 submit.addEventListener('click', (e)=>{
     e.preventDefault();
