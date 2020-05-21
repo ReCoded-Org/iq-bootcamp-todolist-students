b3d17e7cb1c0f0317f3e870b921b6d56fafe453b
Hussien-Talal-Majeed <hossientalal2@gmail.com>
2020-05-21 05:10:20 +0300

    using materialize instead of bootstrap and adding priority input

--- Hussein-Talal/index.js
+++ Hussein-Talal/index.js
@@ -10,17 +10,22 @@ let date = document.getElementById('date');
 const msg = document.getElementById('msg');
 
 //adding the date to the header
-todayDate.textContent = `${day}/${month}/${year}` ;
+
+todayDate.textContent = dateFormater(new Date().toDateString().split(" ")); 
+
+function dateFormater (dateString){
+    return dateString[0]+" "+dateString[1]+ " "+dateString[2] +","+dateString[3];
+}
 
 // setting the min attribute in date input field
-if(day<10){
-    day='0'+day
-} 
-if(month<10){
-    month='0'+month
-} 
-let today = year+'-'+month+'-'+day;
-date.setAttribute("min", today);
+// if(day<10){
+//     day='0'+day
+// } 
+// if(month<10){
+//     month='0'+month
+// } 
+// let today = year+'-'+month+'-'+day;
+// date.setAttribute("min", today);
 
 //adding-task function
 let numberOfTasks = 1;
@@ -33,14 +38,30 @@ function addTask(){
         const div = document.createElement('div');
         const li = document.createElement('li');
         const span = document.createElement('span');
-        div.className = "todo-item";
+        //div.className = "todo-item";
         span.innerText = "deadline : " + date.value;
+        li.className = "collection-item";
         li.innerText = text.value;
         li.appendChild(span);
-        div.appendChild(li);
-        ul.appendChild(div);
+        //div.appendChild(li);
+        ul.appendChild(li);
         numberOfTasks++;
         form.reset();
         msg.textContent = '';
     }    
 }
+
+const submit = document.getElementById('submit');
+submit.addEventListener('click', (e)=>{
+    e.preventDefault();
+})
+
+//initializing materialize components 
+const calendar = document.querySelector('.datepicker');
+M.Datepicker.init(calendar,{
+    minDate: new Date()
+});
+
+let elems = document.querySelectorAll('select');
+let options = document.querySelectorAll('option');
+let instances = M.FormSelect.init(elems, options);
