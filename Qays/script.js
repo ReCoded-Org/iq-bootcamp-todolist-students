e4fd956df083d58efbe8602e27899add3c936b77
Qays <work.worker55@gmail.com>
2020-05-23 19:10:06 +0300

    implement sorting and edit task

--- Qays/script.js
+++ Qays/script.js
@@ -18,19 +18,39 @@ const monthsNames =
 
 const tasksArray = [];
 
-let currentDate = new Date();
+const currentDate = new Date();
+let isEditMode = false;
+let editItemIndex = 0;
 
-let dateH = document.getElementById('current-date');
-dateH.innerHTML = monthsNames[currentDate.getMonth()] + ' ' + currentDate.getDate();
-// dateH.innerText = currentDate.getDate() + '/' + (currentDate.getMonth() +1) +'/'+currentDate.getFullYear();
+const dateH1 = document.getElementById('current-date');
+// Display the current date
+dateH1.innerHTML = monthsNames[currentDate.getMonth()] + ' ' + currentDate.getDate();
 
-let deadlineInput = document.getElementById('deadline');
-let prioritySelector = document.getElementById('priority-selector');
 
-deadlineInput.min = currentDate.toISOString().split('T')[0];
 
-let taskForm = document.getElementById('task-form');
-let showFormButton = document.getElementById('show-form-button');
+const taskForm = document.getElementById('task-form');
+const showFormButton = document.getElementById('show-form-button');
+
+
+const descriptionInput = document.getElementById('description');
+const deadlineInput = document.getElementById('deadline');
+const prioritySelector = document.getElementById('priority-selector');
+
+// init the date picker
+const elems = document.querySelectorAll('.datepicker');
+const instances = M.Datepicker.init(elems, {
+    autoClose: true,
+    minDate: new Date(),
+    format: 'mmm dd, yyyy',
+    onSelect: function (selectedDate) {
+        console.log(selectedDate);
+    }
+});
+
+
+
+
+
 
 
 function showForm() {
@@ -41,45 +61,96 @@ function showForm() {
 function hideForm() {
     taskForm.classList.add('hide');
     showFormButton.style.display = 'block';
+    descriptionInput.value = '';
+    deadlineInput.value = '';
+    prioritySelector.value = 1;
+    isEditMode = false;
+
 
 }
 
 
 function addTask(event) {
-    let description = document.getElementById('description').value;
-    let deadline = deadlineInput.value;
-    let priority = prioritySelector.value;
+    const description = descriptionInput.value;
+    const deadline = deadlineInput.value;
+    const priority = prioritySelector.value;
     if (description === '' || deadline === '') {
         alert('Make sure to fill all of the fields');
         return;
     }
-    tasksArray.push({ description: description, deadline: deadline, done: false, priority: priority });
+
+
+    if (tasksArray.length === 0){
+    tasksArray.push(newTask);
+    return;
+    }
+
+    // Delete the old item if edit is enabled
+    if (isEditMode){
+        tasksArray.splice(editItemIndex,1);
+    }
+    // Adding the new task to its correct timeline position 
+    let lastTime = -1;
+    const newTask = { description: description, deadline: deadline, done: false, priority: priority }
+    const newTaskTime = (new Date(deadline)).getTime();
+    for (let i = 0; i < tasksArray.length; i++) {
+        const taskObject = tasksArray[i];
+        const taskTime = (new Date(taskObject.deadline)).getTime();
+
+        if (lastTime < newTaskTime && taskTime > newTaskTime) {
+
+            tasksArray.splice(i, 0, newTask);
+            break;
+        } else if (i === tasksArray.length - 1) {
+            tasksArray.push(newTask);
+            break;
+
+        }
+
+
+    }
+   
+    saveToStorage();
     drawArray();
 }
 
 
-function drawArray() {
-    localStorage.setItem('myTasks', JSON.stringify(tasksArray));
 
+function editTask(i) {
+    showForm();
+    const taskObject = tasksArray[i];
+
+    descriptionInput.value = taskObject.description;
+    deadlineInput.value = taskObject.deadline;
+    prioritySelector.value = taskObject.priority;
+    isEditMode = true;
+    editItemIndex = i;
+
+}
+
+function drawArray() {
+
+    // Select and delete the old tasks form dom 
     const oldTasks = document.querySelectorAll('#tasks-list *');
     for (let i = 0; i < oldTasks.length; i++) {
         oldTasks[i].remove();
     }
 
+    // Rendering all the tasks to the dom 
     for (let i = 0; i < tasksArray.length; i++) {
         const taskObject = tasksArray[i];
-        let description = taskObject.description;
-        let deadline = taskObject.deadline;
-        let priority = taskObject.priority;
+        const description = taskObject.description;
+        const deadline = taskObject.deadline;
+        const priority = taskObject.priority;
 
-        let task = document.createElement('li');
-        let lineThrough = taskObject.done ? 'line-through' : '';
-        let checked = taskObject.done ? 'checked' : '';
+        const task = document.createElement('li');
+        const lineThrough = taskObject.done ? 'line-through' : '';
+        const checked = taskObject.done ? 'checked' : '';
 
-        let isInThePast = (new Date(taskObject.deadline)).getTime() < currentDate.getTime();
-        let backgroundColor = isInThePast?'red lighten-4 padding s1 rounded-corners':'';
-        let priorityStyles = {'1':'red-text','2':'yellow-text text-darken-2','3':'green-text text-accent-2'}
-        let priorityStyle = priorityStyles[priority.toString()]
+        const isInThePast = (new Date(taskObject.deadline)).getTime() < currentDate.getTime();
+        const backgroundColor = isInThePast ? 'red lighten-4 padding s1 rounded-corners' : '';
+        const priorityStyles = { '1': 'red-text', '2': 'yellow-text text-darken-2', '3': 'green-text text-accent-2' }
+        const priorityStyle = priorityStyles[priority.toString()]
         task.innerHTML = `<div class="row block-underline ">
         <div class="col left">
         <div class="valign-wrapper">
@@ -91,20 +162,36 @@ function drawArray() {
             </div>
             </div>
             </div>
+
             <div class="col right-align right"><i class="small material-icons " onclick="removeTask(${i})">delete</i> </div>
+            <div class="col right-align right"><i class="small material-icons " onclick="editTask(${i})">edit</i> </div>
 
            
             
             </div>`;
         let tasksList = document.getElementById('tasks-list');
         tasksList.appendChild(task);
-        document.getElementById('deadline').value = '';
-        document.getElementById('description').value = '';
-        document.getElementById('no-tasks-message').style.display = 'none';
-    }
-    hideForm();
-}
 
+    }
+
+
+    hideForm();
+    showHideMessage();
+
+}
+function saveToStorage() {
+    localStorage.setItem('myTasks', JSON.stringify(tasksArray));
+
+}
+function showHideMessage() {
+    if (tasksArray.length === 0) {
+        document.getElementById('no-tasks-message').style.display = 'block';
+
+    } else {
+        document.getElementById('no-tasks-message').style.display = 'none';
+
+    }
+}
 function changeState(i) {
     console.log(i);
     console.log(tasksArray[i].done);
@@ -112,38 +199,21 @@ function changeState(i) {
     const taskObject = tasksArray[i];
     taskObject.done = !taskObject.done;
 
-
+    saveToStorage();
     drawArray();
 
 }
 function removeTask(i) {
     tasksArray.splice(i, 1);
+    saveToStorage();
+
     drawArray();
 }
 
-let elems = document.querySelectorAll('.datepicker');
-let instances = M.Datepicker.init(elems, {
-    autoClose: true,
-    minDate: new Date(),
-    // setDefaultDate: new Date(2000,01,31),
-    format: 'mmm dd, yyyy',
-    onSelect: function (datee) {
-        console.log(datee);
 
-    }
-});
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    var elemss = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elemss, {});
-});
-console.log(localStorage.getItem('myTasks'));
-console.log(Array.isArray(localStorage.getItem('myTasks')));
 
 if (Array.isArray(JSON.parse(localStorage.getItem('myTasks')))) {
 
     tasksArray.push(...JSON.parse(localStorage.getItem('myTasks')));
     drawArray();
 }
-// localStorage.clear();
