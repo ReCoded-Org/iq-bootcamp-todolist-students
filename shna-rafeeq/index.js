e43d7da25c09e9a5dcad45229e3c8ec00a7d35a8
shna rafeeq <shnar.majeed@gmail.com>
2020-05-23 20:33:34 +0300

    finalVersion

--- shna-rafeeq/index.js
+++ shna-rafeeq/index.js
@@ -1,55 +1,111 @@
 /*Header*/
 let today = new Date();
 let dd = String(today.getDate()).padStart(2, '0');
-let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+let mm = String(today.getMonth() + 1).padStart(2, '0'); 
 let yyyy = today.getFullYear();
 today = mm + '/' + dd + '/' + yyyy;
-document.getElementById("header").innerHTML = today;
+
+function taskDate(dateMilli) {
+  let d = (new Date(dateMilli) + '').split(' ');
+  d[2] = d[2] + ',';
+
+  return [d[1], d[2], d[3]].join(' ');
+}
+let datemilli = Date.parse(today);
+document.getElementById("header").innerHTML =taskDate(datemilli);
 
 const dateElemnt = document.getElementById("get-date");
 dateElemnt.setAttribute('min', `${yyyy}-${mm}-${dd}`);
 
 let listUl = document.getElementById("list-ul");
 let p = document.getElementById("pul");
-let h3=document.createElement("h3");
-let count=0;
-
 
 let listItemId = 0;
-function newLiAdded(){
-    let gettingTodo = document.getElementById("get-todo").value; 
-    let getdate =document.getElementById("get-date").value;
-    let priorities = document.getElementById("drop-list").value;
+
+const gettingTodo = document.getElementById("get-todo"); 
+const getdate =document.getElementById("get-date");
+const priorities = document.getElementById("drop-list");
+
+let arr = [];
+
+
+function newLiAdded(overrideData = {}, considerEmpty = true){
+
+    const defualtValues = {
+      gettingTodo:  gettingTodo.value,
+      getdate : getdate.value,
+      priorities : priorities.value,
+      completed: false,
+    };
+    const liObject = {
+      ...defualtValues,
+      ...overrideData,
+    }
+
     let frm = document.getElementsByName('contact-form')[0];
     let li=document.createElement('li');
-    li.innerHTML = `<div> <input id="list-item-checkbox-${listItemId}" type="checkbox" onChange="checkboxChanged(${listItemId})" > <span id="list-item-${listItemId}">${gettingTodo} ${getdate} ${priorities}</span> </div> `;
+
+    li.id=`list-item-all-content-${listItemId}`;
+    li.innerHTML = `<div> <input ${ (liObject.completed)? 'checked' : '' } class="checkBox" id="list-item-checkbox-${listItemId}" type="checkbox" onChange="checkboxChanged(${listItemId})" >
+     <span style="${ (liObject.completed)? ' text-decoration: line-through !important;' : '' }" id="list-item-${listItemId}">${liObject.gettingTodo} </span>
+     ${liObject.getdate} ${liObject.priorities} </div>
+    <span class="recycle" id="list-item-button-${listItemId}" onclick="removeLi(${listItemId})">🗑️<span> `;
+    
     let checkFilltext = document.forms["contact-form"]["select-todo"].value;
     let checkFillDtae = document.forms["contact-form"]["selecte-date"].value;
-    if (checkFilltext == "" || checkFilltext == null || checkFillDtae == "" || checkFillDtae == null) {
+    if (considerEmpty &&( checkFilltext == "" || checkFilltext == null || checkFillDtae == "" || checkFillDtae == null ) ) {
         alert("Please fill the all form");
         return false;
       }
     p.remove();
-    count=count+1;
-    h3.innerText=`There Is ${count} Task(s)`;
-    frm.appendChild(h3);
+      //expired date
+    if(new Date(liObject.getdate) < new Date()){
+      li.style.backgroundColor = "#650000";
+    }
+    arr.push(liObject);
+    localStorage.setItem("array", JSON.stringify(arr));
+  
     listUl.appendChild(li);
     frm.reset();
     listItemId++;
 }
+
+
 function checkboxChanged(id){
   let Box = document.getElementById("list-item-checkbox-"+id);
   let paragraph = document.getElementById("list-item-"+id);
+  let arr = JSON.parse(localStorage.getItem("array"));
   if (Box.checked == true){
+    arr[id].completed = true;
     paragraph.style.textDecoration = "line-through";
   } else {
+    arr[id].completed = false;
     paragraph.style.textDecoration = "none";
-  } 
+  }
+  localStorage.setItem("array", JSON.stringify(arr));
+}
+
+function removeLi(id) {
+  let allLi = document.getElementById("list-item-all-content-"+id);
+  allLi.remove();
+  let arr = JSON.parse(localStorage.getItem("array"));
+  arr.splice(id, 1);
+  localStorage.setItem("array", JSON.stringify(arr));
 }
 
 
+function loadStorageLi(){
+  let arr = JSON.parse(localStorage.getItem("array"));
+  arr.forEach((todo)=>{
+    newLiAdded({
+      getdate: todo.getdate,
+      gettingTodo: todo.gettingTodo,
+      priorities: todo.priorities,
+      completed: todo.completed,
 
+    }, false)
+  })
+console.log(arr);
+}
 
-
-
-
+loadStorageLi();
