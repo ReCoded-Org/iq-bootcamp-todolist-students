1d649817369f983ed423c84b65d0a7b6ba840272
Arvin Alhissou <arvin.alhissou@gmail.com>
2020-05-23 23:06:36 +0300

    add some function

--- Arvin/main.js
+++ Arvin/main.js
@@ -1,78 +1,81 @@
-const container = document.querySelector('.container');
-var inputValue = document.querySelector('.input');
-const add = document.querySelector('.add');
+const date = new Date();
+document.getElementById("time").innerHTML = date.toDateString();
+
+let calender = document.querySelectorAll('.datepicker');
+let instances = M.Datepicker.init(calender, {
+    autoClose: true,
+    minDate: new Date(),
+    format: 'mmm dd, yyyy',
+    onSelect: function (datte) {
+        console.log(datte);
+
+    }
+});
 
 if(window.localStorage.getItem("todos") == undefined){
-     var todos = [];
-     //way to set item
-     window.localStorage.setItem("todos", JSON.stringify(todos));
+    var todos = [];
+    //way to set item 
+    window.localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 var todosEX = window.localStorage.getItem("todos");
 var todos = JSON.parse(todosEX);
 
-var date = new Date();
-document.getElementById("time").innerHTML = date.toDateString();
+let todolist = document.getElementById('todoList');
 
-class item{
-	constructor(name){
-		this.createItem(name);
-	}
-    createItem(name){
-    	var itemBox = document.createElement('div');
-        itemBox.classList.add('item');
 
-    	var input = document.createElement('input');
-    	input.type = "text";
-    	input.disabled = true;
-    	input.value = name;
-        input.classList.add('item_input');
-        // for date
-        var datte = document.createElement('input');
-    	datte.type = "date";
-    	datte.disabled = true;
-    	datte.value = name;
-    	datte.classList.add('item_input');
+function addToDo(){
+let dateList =document.getElementById('todoDate').value;
+let textList = document.getElementById('todoText').value;
+let priorityList = document.getElementById('todoPriority').value;
+ 
 
-    	var edit = document.createElement('button');
+
+   let li = document.createElement("li");
+   let text = document.createTextNode(textList);
+   textList.className = "text";
+   li.appendChild(text);
+
+   let date = document.createTextNode(dateList);
+   dateList.className = 'datte';
+   li.appendChild(date);
+    
+   let priority = document.createTextNode(priorityList);
+   priorityList.className = 'priority';
+   li.appendChild(priority);
+
+
+  if (textList === '' || dateList === '') {
+    alert('please fill all your fields !');
+  }
+   else { 
+
+    todolist.appendChild(li);
+  }
+
+    document.getElementById("todoText").value = "";
+
+    let edit = document.createElement('button');
     	edit.classList.add('edit');
     	edit.innerHTML = "EDIT";
-    	edit.addEventListener('click', () => this.edit(input, name));
-
-    	var remove = document.createElement('button');
+        edit.addEventListener('click', () => this.edit(textList));
+        
+    let remove = document.createElement('button');
     	remove.classList.add('remove');
     	remove.innerHTML = "REMOVE";
-    	remove.addEventListener('click', () => this.remove(itemBox, name));
+        remove.addEventListener('click', () => this.remove(textList));
 
-    	container.appendChild(itemBox);
+    let check = document.createElement('input');
+        check.classList.add('check');
+        check.innerHTML = `<input type="checkBox"/>`;
+        check.addEventListener('click', () => this.check(textList));
 
-        itemBox.appendChild(input);
-        itemBox.appendChild(edit);
-        itemBox.appendChild(remove);
+        li.appendChild(check);
+        li.appendChild(edit);
+        li.appendChild(remove);
+} 
 
-    }
-    edit(input, name){
-        if(input.disabled == true){
-           input.disabled = !input.disabled;
-        }
-    	else{
-            input.disabled = !input.disabled;
-            let indexof = todos.indexOf(name);
-            todos[indexof] = input.value;
-            // Saving element in local storage
-            window.localStorage.setItem("todos", JSON.stringify(todos));
-        }
-    }
-
-    remove(itemBox, name){
-        itemBox.parentNode.removeChild(itemBox);
-        let index = todos.indexOf(name);
-        todos.splice(index, 1);
-        window.localStorage.setItem("todos", JSON.stringify(todos));
-    }
-}
-
-add.addEventListener('click', check);
+btn.addEventListener('click', check);
 window.addEventListener('keydown', (e) => {
 	if(e.which == 13){
 		check();
@@ -80,17 +83,39 @@ window.addEventListener('keydown', (e) => {
 })
 
 function check(){
-	if(inputValue.value != ""){
-		new item(inputValue.value);
-        todos.push(inputValue.value);
+	if(textList.value != ""){
+		new item(textList.value);
+        todos.push(textList.value);
         window.localStorage.setItem("todos", JSON.stringify(todos));
-		inputValue.value = "";
+		textList.value = "";
 	}
+}
+remove(li);{
+    itemBox.parentNode.removeChild(itemBox);
+    let index = todos.indexOf(li);
+    todos.splice(index, 1);
+    window.localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+
+edit(textList);{
+    if(input.disabled == true){
+       input.disabled = !input.disabled;
+    }
+    else{
+        input.disabled = !input.disabled;
+        let indexof = todos.indexOf(textList);
+        todos[indexof] = input.value;
+        // Saving element in local storage
+        window.localStorage.setItem("todos", JSON.stringify(todos));
+    }
 }
 
 
 for (var v = 0 ; v < todos.length ; v++){
     new item(todos[v]);
 }
-document.getElementsById('txt').tabIndex = "1";
-document.getElementsById('datte').tabIndex = "0";
+
+
+
+
