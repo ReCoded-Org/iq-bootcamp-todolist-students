16674c1026f5c42d74d5c156b418ae9f90c5fb0b
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-05-24 00:02:18 +0300

    after adding local storage feature

--- abdulbasit-web/script.js
+++ abdulbasit-web/script.js
@@ -4,55 +4,142 @@ const btnDelete = document.querySelector("i.delete");
 const ul = document.querySelector("#task-list");
 const taskCount = document.querySelector("#task-counter");
 const datePicker = document.querySelector("#date-picker");
+const setPriority = document.querySelector("#set-priority");
 
-if(ul.childElementCount==0){
+
+let todoArr = JSON.parse(localStorage.getItem("tasks"))||[];
+
+//empty task list  warning text
+if(todoArr.length==0){
   const h3 = document.createElement("h3");
-  h3.textContent = "You dont have nay task yet ";
+  h3.innerHTML =`<h3 class="text-center ">You don't have any task <i class="fas fa-smile text-pink"></i></h3>`;
   ul.appendChild(h3);
 }
 
+//count tasks
+taskCount.textContent = todoArr.length;
+
+
+
+//add item to array
 btnAdd.addEventListener("click", (e) => {
   e.preventDefault();
+  
   const todo = input.value;
   const deadline = datePicker.value;
+  const priorityValue = setPriority.value;
+//  if(Date.parse(deadline)>Date.parse(new Date())){
+//    console.log("sdvsgh");
+//  }
 
   //check if input is empty
-  if (todo.length > 0) {
-    const task = document.createElement("li");
-    task.className = "list-group-item my-2 rounded-pill";
-
-    task.innerHTML = `<div class="d-flex justify-content-between align-items-center ">
-        <div class="d-flex align-items-center ">
-            <i class="far fa-circle mr-3 pointer" ></i>
-            <p class="m-0">${todo} .</p>
-        </div>
-        <div class="icon">
-            
-        </div>
-    </div>
-    <small class="mb-1 pl-3 ml-3 text-muted ">${deadline}</small>`;
-
-    const deleteIcon = document.createElement("i")
-    deleteIcon.className="far fa-trash-alt text-danger delete  ml-3 pointer"
-    
-    // task.appendChild(btn);
-    ul.appendChild(task);
-    document.querySelector('.icon').appendChild(deleteIcon);
-
-    //clear input after adding
-    input.value = "";
-
-    //count tasks
-    taskCount.textContent = `${ul.childElementCount}`;
-
-    //delete task event listener
-    deleteIcon.addEventListener("click" , ()=>{
-        task.remove();
-    });
+  if (todo.length > 0) {   
+    addItemToArray(todo , deadline , priorityValue);
+    createElement(todoArr[0].todo,todoArr[0].deadline,todoArr[0].priorityValue);
   }
-  
+  //add priority 
+  addPriority(priorityValue);
 
 });
+
+
+
+document.addEventListener("DOMContentLoaded" , ()=>{
+  let reversedArr=todoArr.reverse();
+  for (let todoItem of reversedArr){
+    createElement( todoItem.todo,todoItem.deadline,todoItem.priorityValue)
+  }
+});
+
+
+
+
+
+
+function createElement(todo,deadline,priorityValue){
+    ul.insertAdjacentHTML("afterbegin" , `
+    <li class="list-group-item my-2 rounded-pill pb-1 ">
+    <div class="d-flex justify-content-between align-items-center">
+        <div class="d-flex align-items-center ">
+            <i class="far fa-circle mr-3 pointer"  onClick="doneTask(this)"></i>
+            <p class="m-0">${todo}</p>
+        </div>
+        <div class="icon">
+        <i class="far fa-trash-alt text-danger   ml-3 pointer" onClick="deleteTask(this)"></i>
+        </div>
+    </div>
+    <small class="mb-1 pl-3 ml-3 text-muted ">${deadline}</small>
+    <small class="mb-1 pl-3 ml-3 text-mute " id="priority-value">${priorityValue}</small></li>`);
+
+    //empty the input field
+    input.value = "";
+}
+
+
+
+
+function addItemToArray(todo , deadline , priorityValue){
+  let arrObj = {
+    todo:todo,
+    deadline:deadline,
+    priorityValue:priorityValue
+  };
+
+  todoArr.unshift(arrObj);
+  localStorage.setItem("tasks" , JSON.stringify(todoArr));
+}
+
+
+//delete item from array and DOM
+function deleteTask(x){
+  let listItem = x.closest("li");
+  let paragraph = document.querySelector("p")
+  for(let todoItem of todoArr){
+    if(todoItem.todo == paragraph.innerText){
+      let index = todoArr.indexOf(todoItem);
+      todoArr.splice(index,1);
+      listItem.remove();
+    
+    }
+  }
+  localStorage.setItem("tasks" , JSON.stringify(todoArr));
+  window.location.reload();
+}
+
+
+//complete task
+function doneTask(x){
+  let li = x.closest("li"); 
+  li.classList.toggle("done");
+  x.classList.toggle("text-success");
+  x.classList.toggle("fas");
+  x.parentElement.lastChild.previousElementSibling.classList.toggle("p")
+}
+
+
+
+//add priority
+function addPriority(priorityValue){
+  setPriority.addEventListener("change" , ()=>{
+    if(priorityValue==="High Priority"){
+      // document.querySelector("#priority-value").classList.add("text-danger");
+    }
+    // console.log(priorityValue);
+    
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //data and time setter
@@ -83,17 +170,27 @@ const monthNames = [
   "December",
 ];
 
-time.textContent = `${date.getUTCHours()+3}:${date.getUTCMinutes()}`;
+time.textContent = `${date.getUTCHours()+3}:${date.getUTCMinutes()}`
 day.textContent = `${days[date.getDay()]}, ${date.getDate()}th`;
 month.textContent = `${monthNames[date.getMonth()]}`;
 
+const toda = new Date();
+const tomorrow = new Date(toda);
+tomorrow.setDate(tomorrow.getDate() + 1)
+console.log(tomorrow);
+
+
+
+//Jquery 
 $(document).ready(function () {
   $(input).hide();
   $(datePicker).hide();
+  $(setPriority).hide();
   $(btnAdd).click(() => {
-    $("ul>h3").remove();
-    $(input).fadeIn("slow");
-    $(datePicker).fadeIn("slow");
+    $("ul>h3").fadeOut("fast");
+    $(input).fadeToggle("slow");
+    $(datePicker).fadeToggle("slow");
+    $(setPriority).fadeToggle("slow")
   });
 });
 
@@ -109,9 +206,23 @@ if(dd<10){
     if(mm<10){
         mm='0'+mm
     } 
-
 today = yyyy+'-'+mm+'-'+dd;
 document.getElementById("date-picker").setAttribute("min", today);
 
+
+
+
 // Get the current year for the copyright
 $("#year").text(new Date().getFullYear());
+
+
+
+
+
+
+
+
+
+
+
+
