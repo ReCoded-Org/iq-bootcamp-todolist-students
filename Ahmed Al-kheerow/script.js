8dbb82876102228cae1c8376105ccdab5d70399d
Ahmed Ayad <ahmedayad@Ahmeds-MacBook-Pro.local>
2020-05-24 23:19:53 +0300

    Functionalities are done

--- Ahmed Al-kheerow/script.js
+++ Ahmed Al-kheerow/script.js
@@ -1,86 +1,94 @@
 
+class Task {
 
-    let todayDate = new Date();
+    constructor(description, deadline, priority, isDone = false) {
+        this.description = description;
+        this.deadline = deadline;
+        this.priority = priority;
+        this.isDone = isDone;
+    }
+}
 
-    document.getElementById('today-date').innerHTML = todayDate.getDate() + "/" + (todayDate.getMonth() + 1) + "/" + todayDate.getFullYear();
-    let deadlineInput = document.getElementById('deadline');
-    deadlineInput.min = todayDate.toISOString().split('T')[0]; //to split the date from time, you cant choose previos date
-    console.log(todayDate.toISOString());
+const myTasksArr = [];
 
-    // document.getElementsByClassName('add-task')[0].addEventListener('click', function (event) {
-        // event.preventDefault();
+let todayDate = new Date();
 
-        function addTask(event){
+document.getElementById('today-date').innerHTML = todayDate.getDate() + "/" + (todayDate.getMonth() + 1) + "/" + todayDate.getFullYear();
+let deadlineInput = document.getElementById('deadline');
+deadlineInput.min = todayDate.toISOString().split('T')[0]; //to split the date from time, you cant choose previos date
+console.log(todayDate.toISOString());
 
-           let todo = document.getElementById('todo').value;
-           let deadline = document.getElementById('deadline').value;
-           if (todo === '' || deadline ==='') {
-               alert('Please enter date and todo')
-               return;
-               
-           }
+// document.getElementsByClassName('add-task')[0].addEventListener('click', function (event) {
+// event.preventDefault();
 
-           let myTask = document.createElement('li');
-           myTask.innerHTML = `<div>${todo}</div>  <div>${deadline}</div>`;
+function addTask(event) {
 
-           document.getElementById('todo-tasks').appendChild(myTask);
+    let todo = document.getElementById('todo').value;
+    let deadline = document.getElementById('deadline').value;
+    let priority = document.getElementById('priority').value;
+    if (todo === '' || deadline === '') {
+        alert('Please enter date and todo')
+        return;
 
-           document.getElementById('no-task').style.display = 'none';
-           document.getElementById('todo').value = '';
-           document.getElementById('deadline').value = '';
-           document.getElementById('todo').placeholder = 'GO on';
-           
-        }
-        
+    }
 
+    const task = new Task(todo, deadline, priority);
 
+    myTasksArr.push(task);
+    redrawEverything();
 
-
-
-
-
-
+}
 
 
+function redrawEverything() {
+    localStorage.setItem('myTasks',JSON.stringify(myTasksArr));
 
+    const oldTask = document.querySelectorAll('#todo-tasks div');
 
-
-
-
-
-
-
+    for (let i = 0; i < oldTask.length; i++) {
+        oldTask[i].remove();
+    }
 
 
 
+    for (let i = 0; i < myTasksArr.length; i++) {
+        const task = myTasksArr[i];
+        const done = task.isDone;
+        const classDone = done ? 'del' : '';
+        const checkBox = done ? 'checked' : '';
+        const myTask = document.createElement('div');
+        myTask.innerHTML =
+            ` <div class="input-group-text">
+           <div class="container">
+           <div class="row align-items-center">
+             <div class="col m-0 "><input  type="checkbox" onclick="makeDone(${i})" aria-label="Checkbox for following text input" ${checkBox}></div>
+             <div class="col-md-3 text-left "> <div class="${classDone}">${task.description}</div> <div>${task.deadline},${task.priority}</div></div>
+             <div class="col offset-md-8"><i class="fas fa-trash " onclick="deleteItem(${i})"></i></div>
+           </div>
+       </div>
+       </div>`;
+
+        document.getElementById('todo-tasks').appendChild(myTask);
+
+        document.getElementById('no-task').style.display = 'none';
+        document.getElementById('todo').value = '';
+        document.getElementById('deadline').value = '';
+        document.getElementById('todo').placeholder = 'GO on';
+
+    }
+
+}
+function makeDone(i) {
+    myTasksArr[i].isDone = !myTasksArr[i].isDone;
+
+    redrawEverything();
+}
+function deleteItem(i){
+    myTasksArr.splice(i,1);
+    redrawEverything();
+
+}
 
 
-
-   
-        // function addTask(event) {
-        //     document.getElementById('add-task');
-
-
-        //     let todo = document.getElementById('todo').value;
-        //     let deadline = deadlineInput.value;
-
-        // let theTask = document.createElement('li');
-        // theTask.innerHTML = `<div>${todo}</div><div> ${deadline} </div>`;
-        // document.getElementById('no-task').remove();
-
-        // let newTask = document.getElementById('todo-tasks');
-        // newTask.appendChild(theTask);
-
-        // todo = document.getElementById('todo').placeholder = 'write another one';
-        // todo = document.getElementById('todo').value = '';
-
-        // deadline = document.getElementById('deadline').value = '';
-
-        // }
-        
-
-
-    
-
-
-
+myTasksArr.push(...JSON.parse(localStorage.getItem('myTasks')));
+redrawEverything();
