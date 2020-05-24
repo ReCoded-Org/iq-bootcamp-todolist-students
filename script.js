44a97e0a81f33c51e7bef6d377ada9d14ba288e8
Ahmed-Alassaf <ahmedfaysal199@gmail.com>
2020-05-24 22:19:07 +0300

    Version 2 completed

--- script.js
+++ script.js
@@ -1,25 +1,89 @@
-let n = new Date();
-y = n.getFullYear();
-m = n.getMonth() + 1;
-d = n.getDate();
-document.getElementById("ToDate")
-.innerHTML = m + "/" + d +"/" + y;
+// Elements selection
+const dateToday = document.getElementById('date');
+const list = document.getElementById('list');
+const input = document.getElementById('textInput');
+const button = document.getElementById('button');
+const deadlinedate = document.getElementById('deadline')
+const priorityval = document.getElementById('priority')
 
-document.getElementById("addTodo").addEventListener('click', function(toDoEvent) {
-    let text = document.getElementById("text").value;
-    let deadline = document.getElementById("deadline").value;
-    let addedToDos = document.getElementById("addedToDos");
-    if (text == "", deadline == "") {
-        alert("Add a ToDo!");
+// Check unCheck
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle";
+const LINE_THROUGH = "lineThrough";
+// Variables
+let LIST = []
+    , id = 0;
+// Show date
+const today = new Date();
+options = {weekday : 'long', month : 'short', day : 'numeric'}
+
+dateToday.innerHTML = today.toLocaleDateString('en-US', options)
+
+// Add todo function
+function addToDo(toDo, deadline, priority, id, done, trash){
+
+    if(trash){ return; }
+    
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH : "";
+
+    const item = `<li class="item">
+                    <i class="fas ${DONE} com" job="complete" id="${id}"></i>
+                    <p class="text ${LINE}">${toDo} ${deadline} ${priority}</p>
+                    <i class="fas fa-trash rem" job="remove" id="${id}"></i>
+                  </li>
+                  `;
+    
+    const position = 'beforeend';
+
+    list.insertAdjacentHTML(position, item);
+}
+
+button.addEventListener('click', function(event) {
+    const toDo = input.value
+    const deadline = deadlinedate.value
+    const priority = priorityval.value
+    if (input.value === "" || deadlinedate.value === ""){
+        alert("Add a ToDo & deadline")
+    } else if (toDo){
+        addToDo(toDo, deadline, priority, id, false, false);
+
+        LIST.push({
+            name: toDo,
+            deadline : deadline,
+            priority : priority,
+            id : id,
+            done : false,
+            trash : false
+        });
+
+        id++
     }
-
-    else {
-        
-        let newToDo = document.createElement('li');
-        newToDo.innerHTML = `<div>${text}</div><div>${deadline}</div>`;
-        addedToDos.appendChild(newToDo);
-
-        text = document.getElementById('text').value ='';
-        deadline = document.getElementById('deadline').value ='';
-    }
+    input.value = ""
+    deadlinedate.value = ""
 });
+
+// Check and Uncheck toDo
+function completeToDo(element){
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+
+    LIST[element.id].done = LIST[element.id].done ? false : true;
+}
+// remove toDo
+function removeToDo(element){
+    element.parentNode.parentNode.removeChild(element.parentNode);
+
+    LIST[element.id].trash = true;
+}
+
+list.addEventListener('click', function(event) {
+    const element = event.target;
+    const elementJob = element.attributes.job.value;
+    if (elementJob == "complete"){
+        completeToDo(element);
+    } else if (elementJob == "remove"){
+        removeToDo(element);
+    }
+})
