a93c0ab18de348dd6f71c357b4abbe4f8a0288f9
Shirin Muataz <shirinmuataz@gmail.com>
2020-05-26 02:46:40 +0300

    Fixed Local Storage Problem... FINALLYyyyyyy!!!!!!!!!!

--- Shirin Muataz/script.js
+++ Shirin Muataz/script.js
@@ -1,14 +1,18 @@
 let masterList = [];
 window.onload = function () {
-  if (masterList !== null) {
+  if (localStorage.length > 1) {
     for (let j = 0; j < localStorage.length; j++) {
       masterList[j] = JSON.parse(localStorage.getItem("task" + j)); //get data from storage
-      let storedTask = masterList[j];
-      let entry = document.createElement("li"); 
-      let list = document.getElementById("ulmenu"); 
-      entry.innerHTML = storedTask; 
-      list.appendChild(entry); 
+      if ((typeof masterList[j] !== 'undefined') && (masterList[j] !== null)) {
+        let storedTask = masterList[j];
+        let entry = document.createElement("li");
+        let list = document.getElementById("ulmenu");
+        entry.innerHTML = storedTask;
+        list.appendChild(entry);
+
+      }
     }
+
   } else {
     //if nothing exist in storage, keep todos array empty
     masterList = [];
@@ -30,60 +34,36 @@ document.getElementById("newTask-Date").setAttribute("min", today);
 
 //----------Adding Task to List Function --------------
 function addTask() {
-  //------------Changing Month format to Manth's Name ------------
-  let date = new Date($("#newTask-Date").val());
-  let dateArr = changeMonthFormat(date); //----call function that changes Month's format
-  let task = { task: document.getElementById("newTask").value };
-  let ul = document.getElementById("ulmenu");
+  if (document.getElementById("newTask-Date").value !== "" && document.getElementById("newTask") !== "") {
+    let date = new Date($("#newTask-Date").val());
+    option = { month: "long", day: "numeric", year: "numeric" };
+    let dateArr = { date: date.toLocaleDateString("en-US", option) };
+    let task = { task: document.getElementById("newTask").value };
+    let ul = document.getElementById("ulmenu");
 
-  //--------------- Defining the Array of Objects
-  let arrObj = [dateArr, task];
-  let li = document.createElement("li");
-  let inner = `${arrObj[1].task}`;
-  inner += `<button class="btn float-right" onclick="delItem()"><i class="fa fa-trash delete_icon" style="font-size:1.5rem;color:red"></i></button>`;
-  inner +=
-    '<button class="btn float-right" onclick="doneItem()"><i class="fa fa-check-circle" style="font-size:1.5rem;color:green"></i></button>';
-  inner += `<br>`;
-  inner +=
-    `${arrObj[0][0].month}` +
-    "-" +
-    `${arrObj[0][1].day}` +
-    "-" +
-    `${arrObj[0][2].year}`;
-  li.innerHTML = inner;
-  $("#ulmenu").append(inner);
-  clearInput();
-  masterList.push(inner);
-  //--------------Adding the task -------------------
-  for (let j = 0; j < masterList.length; j++)
-    window.localStorage.setItem("task" + j, JSON.stringify(masterList[j]));
+    //--------------- Defining the Array of Objects
+    let arrObj = [dateArr, task];
+    let li = document.createElement("li");
+    let inner = `${arrObj[1].task}`;
+    inner += `<button class="btn float-right" onclick="delItem()"><i class="fa fa-trash delete_icon" style="font-size:1.5rem;color:red"></i></button>`;
+    inner +=
+      '<button class="btn float-right" onclick="doneItem()"><i class="fa fa-check-circle" style="font-size:1.5rem;color:green"></i></button>';
+    inner += `<br>`;
+    inner += `${arrObj[0].date}`;
+    li.innerHTML = inner;
+    $("#ulmenu").append(li);
+    clearInput();
+    masterList.push(inner);
+    //--------------Adding the task -------------------
+    for (let j = 0; j < masterList.length; j++)
+      window.localStorage.setItem("task" + j, JSON.stringify(masterList[j]));
+  }
+  else
+    alert("Please Enter Task Date and Task");
 }
 function clearInput() {
   document.getElementById("form").reset();
 }
-function changeMonthFormat(date) {
-  const monthArr = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  let day = { day: date.getDate() };
-  let month = date.getMonth();
-  let year = { year: date.getFullYear() };
-  let monthName = { month: monthArr[month] };
-  let dateAfterFormat = [monthName, day, year];
-  return dateAfterFormat;
-}
-
 function delItem() {
   event.target.closest("li").remove();
 }
@@ -92,3 +72,7 @@ function doneItem() {
   event.target.closest("li").setAttribute("class", "selected");
 }
 
+function clearLocalMem() {
+  localStorage.clear();
+  $("#ulmenu").empty();
+}
